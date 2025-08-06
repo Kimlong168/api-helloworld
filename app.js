e35c6631612b50e54b0cfd763656aeb162672062
CHANN KIMLONG <kimlong5244@gmail.com>
2025-08-06 11:55:58 +0700

    fix: update cors

--- app.js
+++ app.js
@@ -9,6 +9,25 @@ const PORT = process.env.PORT || 3001; // 👈 use env port if available
 
 app.use(bodyParser.json());
 
+
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const allowedOrigins = process.env.CORS_ORIGINS?.split(',') || [];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
+
+
 db.sequelize.sync().then(() => {
   console.log("Synced DB.");
 });
